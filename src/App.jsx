a6ae07a635c9ee144a16257af7744505743118e5
PintilieGeani <pintilie.geany@yahoo.it>
2025-09-01 18:08:09 +0200

    feat: Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
 
@@ -8,7 +8,15 @@ function App() {
   const [spec, setSpec] = useState("")
   const [exp, setExp] = useState(0)
   const [description, setDescription] = useState("")
-  
+  const [pwdValid, setPwdValid] = useState(false)
+  const [userValid, setUserValid] = useState(false)
+  const [descriptionValid, setDescriptionValid] = useState(false)
+
+  // Array per le validazioni della password
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*()-_=+[]{}|; :'\\,.<>?/`~";
+
   const handlePassword = (e) => {
     e.preventDefault()
     setPassword(e.target.value)
@@ -16,22 +24,22 @@ function App() {
 
   const handleExp = (e) => {
     e.preventDefault()
-    if(e.target.value < 0){
+    if (e.target.value < 0) {
       console.log("Esperienza negativa")
       return
-    }else{
+    } else {
       setExp(e.target.value)
     }
   }
 
-   const handleSpec = (e) => {
+  const handleSpec = (e) => {
     e.preventDefault()
-      setSpec(e.target.value)
+    setSpec(e.target.value)
   }
 
   const handleForm = (e) => {
     e.preventDefault()
-    if(spec === ""){
+    if (spec === "") {
       console.log("Seleziona la Specializzazione")
       return
     }
@@ -45,59 +53,96 @@ function App() {
     })
   }
 
+
+
+  useEffect(() => {
+    const validazionePwd = () => {
+      const hasNumber = numbers.split("").some((num) => password.includes(num));
+      const hasLetter = letters.split("").some((lett) => password.includes(lett));
+      const hasSymbol = symbols.split("").some((elem) => password.includes(elem));
+      if(hasNumber && hasLetter && hasSymbol){
+        setPwdValid(true)
+      }
+    };
+
+    validazionePwd();
+  }, [password]);
+
+  useEffect(() => {
+    const validazioneUsername = () => {
+      const hasSymbol = symbols.split("").some((elem) => username.includes(elem))
+      if(username.length > 6 && hasSymbol === false){
+        setUserValid(true)
+      }else{
+        setUserValid(false)
+      }
+    }
+    validazioneUsername()
+  }, [username])
+
+  useEffect(() => {
+    if(description.trim().length > 100 && description.trim().length < 1000){
+      setDescriptionValid(true)
+    }else{
+      setDescriptionValid(false)
+    }
+  },[description])
+
   return (
     <>
       <div className="container">
         <form onSubmit={handleForm}>
-        <input 
-        type="text"
-        placeholder="Nome Completo"
-        value={nome}
-        onChange={(e) => setNome(e.target.value)}
-        />
-        <input 
-        type="text"
-        autoComplete="username"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        />
-        <input 
-        type="password"
-        placeholder="Password"
-        autoComplete="current-password"
-        value={password}
-        onChange={handlePassword}
-        />
-        <select 
-        name="Seleziona specializzazione"
-        id="Spec"
-        value={spec}
-        onChange={handleSpec}
-        >
-          <option value="">Specializzazione</option>
-          <option value="Front-end">Front-end</option>
-          <option value="Back-end">Back-end</option>
-          <option value="Full Stack">Full Stack</option>
-        </select>
-        <input 
-        type="number"
-        placeholder="Anni di esperienza"
-        min={1}
-        value={exp}
-        onChange={handleExp}
-        />
-        <textarea 
-        name="Text-area" 
-        id="txt"
-        placeholder="Breve descrizione dello sviluppatore"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        >
+          <input
+            type="text"
+            placeholder="Nome Completo"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <input
+            type="text"
+            autoComplete="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <p className={userValid? "valid" : "invalid"}>{userValid ? "Username Valido" : "Username deve contenere almeno 6 caratteri alfanumerici senza simboli o spazi"}</p>
+          <input
+            type="password"
+            placeholder="Password"
+            autoComplete="current-password"
+            value={password}
+            onChange={handlePassword}
+          />
+          <p className={password.length > 0 && pwdValid ? "valid" : "invalid"}>{password.length > 0 && pwdValid ? "Password valida" : "La password deve contenere almeno un numero"}</p>
+          <select
+            name="Seleziona specializzazione"
+            id="Spec"
+            value={spec}
+            onChange={handleSpec}
+          >
+            <option value="">Specializzazione</option>
+            <option value="Front-end">Front-end</option>
+            <option value="Back-end">Back-end</option>
+            <option value="Full Stack">Full Stack</option>
+          </select>
+          <input
+            type="number"
+            placeholder="Anni di esperienza"
+            min={1}
+            value={exp}
+            onChange={handleExp}
+          />
+          <textarea
+            name="Text-area"
+            id="txt"
+            placeholder="Breve descrizione dello sviluppatore"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          >
+          </textarea>
+          <p className={descriptionValid ? "valid" : "invalid"}>{descriptionValid ? "Descrizione valida" : "La descrizione deve contenere almeno 100 caratteri ma meno di 1000"}</p>
 
-        </textarea>
-
-        <button type="submit">Submit</button>
+          <button type="submit">Submit</button>
         </form>
 
       </div>
